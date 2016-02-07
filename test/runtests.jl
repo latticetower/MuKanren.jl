ec740391b9cd7652a13c34b27fed8e5d152b8b9e
Tatiana Malygina <merlettaia@gmail.com>
2016-02-07 21:30:04 +0300

    wip

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,25 +1,25 @@
 import Base.string
 using Base.Test, miniKanren
-
+importall miniKanren
 
 print(Pair(1,2))
 
-@test miniKanren.is_cons(list(1,2,3))
+@test is_cons(list(1,2,3))
 
 println("all fine")
 
-@test miniKanren.ext_s("a", 1, nil()) == list(Pair("a", 1))
+@test ext_s("a", 1, nil()) == list(Pair("a", 1))
 
-res = miniKanren.assp(x->(x=="a"), list(Pair("b", 2), Pair("a", 1)))
+res = assp(x->(x=="a"), list(Pair("b", 2), Pair("a", 1)))
 println(res)
 # @test res == list(Pair("a", 1))
 
 empty_state=Pair(nil(), 0)
 call_empty_state(g) = g(empty_state)
 
-five = x -> miniKanren.equals(x, 5)
-fives = x -> miniKanren.disj(miniKanren.equals(x, 5), () -> fives(x))
-println((miniKanren.call_fresh(five))(empty_state))
+five = x -> equals(x, 5)
+fives = x -> disj(equals(x, 5), () -> fives(x))
+println((call_fresh(five))(empty_state))
 
 
 include("macroTests.jl")
