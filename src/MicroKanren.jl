617a7317a28679a30abbaafd495a40267a693dca
Tatiana Malygina <merlettaia@gmail.com>
2016-02-24 20:26:49 +0300

    take n, take all return array instead of cons

--- src/MicroKanren.jl
+++ src/MicroKanren.jl
@@ -1,6 +1,6 @@
 
 module MicroKanren
-export is_cons, assp, ext_s, call_fresh, map, length, equals, list, nil, Nil, bind, mplus, mzero, Var, var, conj, disj, car, cdr, take, take_all, vars_equal, cons
+export is_cons, assp, ext_s, call_fresh, length, equals, list, nil, Nil, bind, mplus, mzero, Var, var, conj, disj, car, cdr, take, take_all, vars_equal, cons
 #helpers
 #car{T}(x :: Cons{T}) = head(x)
 #cdr{T}(x :: Cons{T}) = tail(x)
@@ -9,8 +9,14 @@ immutable Nil
 end
 nil() = Nil()
 
+immutable Var
+  index :: Int
+end
+
 car(x :: Pair) = first(x)
 cdr(x :: Pair) = last(x)
+car(x :: Array) = x[1]
+cdr(x :: Array) = x[2 : end]
 
 
 is_cons(d) = isa(d, Pair) && isa(cdr(d), Pair)
@@ -25,7 +31,7 @@ end
 
 
 is_procedure(elt) = isa(elt, Function)
-typealias Var{T} Vector{T}
+#typealias Var{T} Vector{T}
 #  (define (assp p l)
 #    (cond ((null? l) #f)
 #          ((p (car l)) (car l))
@@ -43,15 +49,15 @@ end
 
 #code
 
-var(c) = [c]
-is_var(x) = isa(x, Var)
+#Var(c) = [c]
+is_Var(x) = isa(x, Var)
 
-vars_equal(x1, x2) = x1[1] == x2[1]
+vars_equal(x1, x2) = x1.index == x2.index
 
 
 function walk(u, s)
   #println(string("walk: ", u, " || ", s))
-  if is_var(u)
+  if is_Var(u)
     pr = assp(v -> vars_equal(u, v), s)
     #println(string("assp call result: ", pr))
     pr!=nil() && return walk(cdr(pr), s)
@@ -74,10 +80,10 @@ function unify(u, v, s)
   #println(string("++++++in unify call ", u, " ",  v, " ", s))
   u = walk(u, s)
   v = walk(v, s)
-  #println(string("unify call-after walk ", u, " ",  v, " ", is_var(u), is_var(v), is_pair(u), is_pair(v)))
-  is_var(u) && is_var(v) && vars_equal(u, v) && return s
-  is_var(u) && return ext_s(u, v, s)
-  is_var(v) && return ext_s(v, u, s)
+  #println(string("unify call-after walk ", u, " ",  v, " ", is_Var(u), is_Var(v), is_pair(u), is_pair(v)))
+  is_Var(u) && is_Var(v) && vars_equal(u, v) && return s
+  is_Var(u) && return ext_s(u, v, s)
+  is_Var(v) && return ext_s(v, u, s)
   if is_pair(u) && is_pair(v)
     #println("before recursive unify call")
     s = unify(car(u), car(v), s)
@@ -97,7 +103,7 @@ function call_fresh(f)
   s_c -> begin
     c = cdr(s_c)
     #println(string("call_fresh ", s_c))
-    f(var(c))(Pair(car(s_c), c + 1))
+    f(Var(c))(Pair(car(s_c), c + 1))
   end
 end
 
@@ -131,7 +137,7 @@ bind(stream :: Pair, goal :: Any) = mplus(goal(car(stream)), bind(cdr(stream), g
 import Base.string
 
 string(p :: Pair) = last(p) != nil() ? Base.string("(", p[1], " . ", p[2],")") : Base.string("(", p[1],")")
-string(v :: Var) = Base.string("#(", v[1], ")")
+string(v :: Var) = Base.string("#(", v.index, ")")
 string(v :: Nil) = Base.string("#(nil)")
 
 
@@ -196,15 +202,15 @@ end
 function take_all(s)
   s = pull(s)
   #println(s)
-  s == nil() && return nil()
-  cons(car(s), take_all(cdr(s)))
+  s == nil() && return []
+  [car(s), take_all(cdr(s))...]
 end
 
 function take(n, s)
-  n == 0 &&  return nil()
+  n == 0 &&  return []
   s = pull(s)
-  s == nil() && return nil()
-  cons(car(s), take(n - 1, cdr(s)))
+  s == nil() && return []
+  [car(s), take(n - 1, cdr(s))...]
 end
 
 
@@ -216,7 +222,7 @@ reify_name(n :: Int) = symbol("_.", n)
 length(n :: Nil) = 0
 length(p:: Pair) = cdr(p) == nil() && car(p) == nil() ? 0 : (car(p)!= nil() ? 1: 1 + length(cdr(p)))
 
-function map(f :: Function, p)
+function map(f :: Function, p :: Pair)
   p == nil() && return nil()
   Pair(f(car(p)), map(f, cdr(p)))
 end
@@ -224,7 +230,7 @@ end
 function reify_s(v, s)
   v = walk(v, s)
   #println(s)
-  is_var(v) && return cons(Pair(v, reify_name(length(s))), s)
+  is_Var(v) && return cons(Pair(v, reify_name(length(s))), s)
   is_pair(v) && return reify_s(cdr(v), reify_s(car(v), s))
   s
 end
@@ -233,18 +239,18 @@ end
 function walk_star(v, s)
   #println(string("walk star", v, s))
   v = walk(v, s)
-  is_var(v) && return v
+  is_Var(v) && return v
   is_pair(v) && return cons(walk_star(car(v), s), walk_star(cdr(v), s))
   v
 end
 
-mk_reify(s_cs) = map(reify_state_1st_var, s_cs)
+mk_reify(s_cs) = Base.map(reify_state_1st_var, s_cs)
 
-export reify_state_1st_var
+export reify_state_1st_var, mk_reify
 
 
-function reify_state_1st_var(s_c)
-  v = walk_star([0], car(s_c))
+function reify_state_1st_var(s_c :: Pair)
+  v = walk_star(Var(0), car(s_c))
   walk_star(v, reify_s(v, nil()))
 end
 
@@ -263,7 +269,7 @@ end
 
 function occurs(x, v, s)
   v = walk(v, s)
-  is_var(v) && return vars_equal(v, x)
+  is_Var(v) && return vars_equal(v, x)
   is_pair(v) && (occurs(x, car(v), s) || occurs(x, cdr(v), s))
 end
 #next is module end
--- test/funcTests.jl
+++ test/funcTests.jl
@@ -15,16 +15,16 @@ facts("Goal construction") do
   end
 
   context("assp") do
-    res = assp(x->vars_equal(x, [1]), list(Pair([1], "b"), Pair([0], "a")))
-    @fact res --> Pair([1], "b")
-    res = assp(x ->vars_equal(x, [3]), list(Pair([4], 6), Pair([3], 5), Pair([2], 6), Pair([1], 5)) )
-    @fact res --> Pair([3], 5)
+    res = assp(x->vars_equal(x, Var(1)), list(Pair(Var(1), "b"), Pair(Var(0), "a")))
+    @fact res --> Pair(Var(1), "b")
+    res = assp(x ->vars_equal(x, Var(3)), list(Pair(Var(4), 6), Pair(Var(3), 5), Pair(Var(2), 6), Pair(Var(1), 5)) )
+    @fact res --> Pair(Var(3), 5)
   end
 
   empty_state=Pair(nil(), 0)
   call_empty_state(g) = g(empty_state)
-  some_state = Pair(list(Pair([0], 6)), 1)
-  some_state2 = Pair(list(Pair([0], 5)), 1)
+  some_state = Pair(list(Pair(Var(0), 6)), 1)
+  some_state2 = Pair(list(Pair(Var(0), 5)), 1)
   five = x -> equals(x, 5)
   fives = x -> disj(equals(x, 5), s_c -> () -> fives(x)(s_c))
   a_and_b = conj(
@@ -36,12 +36,12 @@ facts("Goal construction") do
     )
 
   context("call/fresh") do
-    @fact call_fresh(five)(empty_state) --> list(Pair(list(Pair([0], 5)), 1))
-    @fact call_fresh(five)(some_state) --> list(Pair(list(Pair([1], 5), Pair([0], 6)), 2))
-    @fact call_fresh(five)(some_state2) --> list(Pair(list(Pair([1], 5), Pair([0], 5)), 2))
+    @fact call_fresh(five)(empty_state) --> list(Pair(list(Pair(Var(0), 5)), 1))
+    @fact call_fresh(five)(some_state) --> list(Pair(list(Pair(Var(1), 5), Pair(Var(0), 6)), 2))
+    @fact call_fresh(five)(some_state2) --> list(Pair(list(Pair(Var(1), 5), Pair(Var(0), 5)), 2))
     #println(take(1, call_fresh(q-> fives(q))(empty_state)))
-    @fact take(1, call_fresh(q-> fives(q))(empty_state)) --> list(Pair(list(Pair([0], 5)), 1))
-    @fact cdr(take(2, call_fresh(x -> fives(x))(empty_state))) --> list(Pair(list(Pair([0], 5)), 1))
+    @fact take(1, call_fresh(q-> fives(q))(empty_state)) --> [Pair(list(Pair(Var(0), 5)), 1)]
+    @fact cdr(take(2, call_fresh(x -> fives(x))(empty_state))) --> [Pair(list(Pair(Var(0), 5)), 1)]
     #fives = x -> disj(equals(x, 5), s_c ->() -> ((fives(x))(s_c)))
     #println((call_fresh(fives))(empty_state))
     ccc = call_fresh(a-> equals(a, :([1, 2])))
@@ -50,18 +50,18 @@ facts("Goal construction") do
 
   context("a-and-b") do
     a_and_b_result = a_and_b(empty_state)
-    @fact car(a_and_b_result) --> Pair(list(Pair([1], 5), Pair([0], 7)), 2)
-    @fact take(1, a_and_b_result) --> list(Pair(list(Pair([1], 5), Pair([0], 7)), 2))
-    @fact car(cdr(a_and_b_result)) --> Pair(list(Pair([1], 6), Pair([0], 7)), 2)
+    @fact car(a_and_b_result) --> Pair(list(Pair(Var(1), 5), Pair(Var(0), 7)), 2)
+    @fact take(1, a_and_b_result) --> [Pair(list(Pair(Var(1), 5), Pair(Var(0), 7)), 2)]
+    @fact car(cdr(a_and_b_result)) --> Pair(list(Pair(Var(1), 6), Pair(Var(0), 7)), 2)
     @fact cdr(cdr(a_and_b_result)) --> nil()
 
-    @fact take(2, a_and_b(empty_state)) --> list(
-      Pair(list(Pair([1], 5), Pair([0], 7)), 2),
-      Pair(list(Pair([1], 6), Pair([0], 7)), 2))
+    @fact take(2, a_and_b(empty_state)) --> [
+      Pair(list(Pair(Var(1), 5), Pair(Var(0), 7)), 2),
+      Pair(list(Pair(Var(1), 6), Pair(Var(0), 7)), 2)]
 
-    @fact take_all(a_and_b(empty_state)) --> list(
-      Pair(list(Pair([1], 5), Pair([0], 7)), 2),
-      Pair(list(Pair([1], 6), Pair([0], 7)), 2))
+    @fact take_all(a_and_b(empty_state)) --> [
+      Pair(list(Pair(Var(1), 5), Pair(Var(0), 7)), 2),
+      Pair(list(Pair(Var(1), 6), Pair(Var(0), 7)), 2)]
   end
 
   context("bind and mplus") do
@@ -71,14 +71,14 @@ facts("Goal construction") do
 
   context("conj and disj") do
     exp1 = call_fresh(b->call_fresh(a->conj(equals(a, 3), equals(b, 4))))
-    @fact exp1(empty_state) --> list(Pair(list(Pair([0], 4), Pair([1], 3)), 2))
+    @fact exp1(empty_state) --> list(Pair(list(Pair(Var(0), 4), Pair(Var(1), 3)), 2))
     expression = conj(call_fresh(a->equals(a, 7)), call_fresh(b->disj(equals(b, 5), equals(b, 6))))
     @fact expression(empty_state) --> list(
-      Pair(list(Pair([1], 5), Pair([0], 7)), 2),
-      Pair(list(Pair([1], 6), Pair([0], 7)), 2))
+      Pair(list(Pair(Var(1), 5), Pair(Var(0), 7)), 2),
+      Pair(list(Pair(Var(1), 6), Pair(Var(0), 7)), 2))
     exp3 = call_fresh(a->disj(equals(a, 3), equals(a, 4)))
     println(exp3(empty_state))
-    @fact exp3(empty_state) --> list(Pair(list(Pair([0], 3)), 1), Pair(list(Pair([0], 4)), 1))#TODO: fix
+    @fact exp3(empty_state) --> list(Pair(list(Pair(Var(0), 3)), 1), Pair(list(Pair(Var(0), 4)), 1))#TODO: fix
   end
 
   context("cons construction") do
@@ -155,39 +155,42 @@ facts("Function calls") do
   end
 
   context("ground_appendo") do
-    @fact car(ground_appendo(empty_state)()) --> Pair(list(list([2], "b"), Pair([1], nil()), Pair([0], "a")), 3)
+    @fact car(ground_appendo(empty_state)()) --> Pair(list(list(Var(2), "b"), Pair(Var(1), nil()), Pair(Var(0), "a")), 3)
   end
 
   context("ground_appendo2") do
     @fact car(ground_appendo2(empty_state)()) -->
-      Pair(list(list([2], "b"), list([1]), Pair([0], "a")), 3)
+      Pair(list(list(Var(2), "b"), list(Var(1)), Pair(Var(0), "a")), 3)
   end
 
   context("appendo") do
-    @fact take(2, call_appendo(empty_state)) --> list(
-        Pair(list(list([0], [1], [2], [3]), Pair([2], [3]), list([1])), 4),
-        Pair(list(list([0], [1], [2], [3]), Pair([2], [6]), list([5]),
-          cons([3], cons([4], [6])), cons([1], cons([4], [5]))), 7)
-    )
+    @fact take(2, call_appendo(empty_state)) --> [
+        Pair(list(list(Var(0), Var(1), Var(2), Var(3)), Pair(Var(2), Var(3)), list(Var(1))), 4),
+        Pair(list(list(Var(0), Var(1), Var(2), Var(3)), Pair(Var(2), Var(6)), list(Var(5)),
+          cons(Var(3), cons(Var(4), Var(6))), cons(Var(1), cons(Var(4), Var(5)))), 7)
+    ]
   end
 
   context("appendo2") do
-    @fact take(2, call_appendo2(empty_state)) --> list(
-      Pair(list(list([0], [1], [2], [3]), Pair([2], [3]), list([1])), 4),
-      Pair(list(list([0], [1], [2], [3]), cons([3], cons([4], [6])),
-        Pair([2], [6]), list([5]), cons([1], cons([4], [5]))), 7)
-    )
+    @fact take(2, call_appendo2(empty_state)) --> [
+      Pair(list(list(Var(0), Var(1), Var(2), Var(3)), Pair(Var(2), Var(3)), list(Var(1))), 4),
+      Pair(list(list(Var(0), Var(1), Var(2), Var(3)), cons(Var(3), cons(Var(4), Var(6))),
+        Pair(Var(2), Var(6)), list(Var(5)), cons(Var(1), cons(Var(4), Var(5)))), 7)
+    ]
   end
 
   context("reify-1st across appendo") do
-  res = take(2, call_appendo(empty_state))
-
-  @fact string(map(reify_state_1st_var, res)) --> string(list(
-    list(nil(), "_.0", "_.0"),
-    list(list("_.0"), "_.1", cons("_.0", "_.1"))))
-  @fact string(map(reify_state_1st_var, res)) --> string(list(
-    list(nil(), "_.0", "_.0"),
-    list(list("_.0"), "_.1", cons("_.0", "_.1"))))
+    res = take(2, call_appendo(empty_state))
+  println(res)
+    @fact string(map(reify_state_1st_var, res)) --> string([
+      list(nil(), "_.0", "_.0"),
+      list(list("_.0"), "_.1", cons("_.0", "_.1"))])
+    @fact string(map(reify_state_1st_var, res)) --> string([
+      list(nil(), "_.0", "_.0"),
+      list(list("_.0"), "_.1", cons("_.0", "_.1"))])
+    @fact string(mk_reify(res)) --> string([
+      list(nil(), "_.0", "_.0"),
+      list(list("_.0"), "_.1", cons("_.0", "_.1"))])
   end
 
   context("many_non_ans") do
@@ -206,7 +209,7 @@ facts("Function calls") do
     end
 
     many_non_ans = call_fresh(x -> disj(relo(cons(5, 6)), equals(x, 3)))
-    @fact take(1, many_non_ans(empty_state)) --> list(Pair(list(Pair([0], 3)), 1))
+    @fact take(1, many_non_ans(empty_state)) --> [Pair(list(Pair(Var(0), 3)), 1)]
   end
 
 
